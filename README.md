3190aca69afea968faf79bfe9f11cf6fd4d3e8d5
Michał Kurc <mikeqrc@gmail.com>
2024-07-19 13:33:45 +0200

    chore: Add start script for easy application setup and execution

---
+++ README.md
@@ -0,0 +1,13 @@
+### To run the application, execute the following command:
+
+1. Ensure `start.sh` has execute permissions:
+
+    ```sh
+    chmod +x ./start.sh
+    ```
+
+2. Run the script:
+
+    ```sh
+    ./start.sh
+    ```
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create Python virtual environment
+echo 'Creating Python virtual environment ".venv" in agents directory'
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install dependencies from "requirements.txt" into the virtual environment
+echo 'Installing dependencies from "requirements.txt" into virtual environment'
+pip install -r requirements.txt
+
+# Run the Streamlit application
+echo 'Running the Streamlit application'
+streamlit run streamlit_app.py --server.port 7860
